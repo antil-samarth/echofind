95e8d13c15b5ae4fb8321ed48eeb5dc71d29d0f8
Samarth <antil.samarth@gmail.com>
2025-04-30 15:52:30 +0530

    Implemented - read samples, check count, convert stereo to mono

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use hound;
-/* This is a project about audio fingerprinting */
+
 fn main() {
     println!("Hello, world!");
-    let reader = hound::WavReader::open("src/media/wav/01_Genesis.wav").unwrap();
+    let mut reader = hound::WavReader::open("src/media/wav/01_Genesis.wav")
+        .expect("Failed to open file");
     println!("file name - 01_Genesis.wav");
     let spec = reader.spec();
     println!("Sample rate: {}", spec.sample_rate);
@@ -14,4 +15,27 @@ fn main() {
         "Duration: {} seconds",
         reader.duration() as f32 / spec.sample_rate as f32
     );
+
+    let samples_result: Result<Vec<i16>, hound::Error> = reader.samples::<i16>().collect();
+    let mut samples_vec = samples_result.expect("Failed to read all samples");
+
+    println!("Number of samples read: {}", samples_vec.len());
+    println!("First 10 samples: {:?}", &samples_vec[0..10]);
+
+    if samples_vec.len() != reader.len() as usize {
+        panic!("Number of samples read does not match the total number of samples");
+    }
+
+    if spec.channels == 2 {
+        println!("This is a stereo file");
+        println!("Converting to mono");
+
+        samples_vec = samples_vec
+            .chunks_exact(2)
+            .map(|chunk: &[i16]| ((chunk[0] as i32 + chunk[1] as i32) / 2) as i16)
+            .collect();
+
+        println!("Number of samples read: {}", samples_vec.len());
+        println!("First 10 samples: {:?}", &samples_vec[0..10]);
+    }
 }
